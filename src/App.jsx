2bdcbd45e5c208a31ead77152c6dd06de8efcb86
Marilin Francisco <f.marilin@wustl.edu>
2025-04-04 13:19:29 -0500

    added squares interact element: console printing ('clicked')

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,19 @@ function App() {
 export default App
 */
 
-function Square({value}) {
-  return <button className="square">{value}</button>;
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 export default function Board() {
   return (

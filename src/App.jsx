03351ce12ed5d07728f7b2075c73affe7f82443d
Marilin Francisco <f.marilin@wustl.edu>
2025-04-04 14:21:47 -0500

    buttons stating history are now usable to jump to version of button pressed

--- src/App.jsx
+++ src/App.jsx
@@ -120,15 +120,20 @@ export default function Game(){
 
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares){
-    setHistory([...history, nextSquares])
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove){
-
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
@@ -142,7 +147,7 @@ export default function Game(){
     }
 
     return(
-      <li>
+      <li key={move}>
         <button onClick={ () => jumpTo(move)}>
           {description}
         </button>
